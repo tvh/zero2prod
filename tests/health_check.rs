925c7fe1b6f5d8d389a4606ccce9e31f8c623d58
Timo von Holtz <tvh@tvholtz.de>
2023-09-17 20:41:42 -0500

    Add functioning test for health_check

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,23 @@
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener =
+        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request");
