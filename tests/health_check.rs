af1ee835cf2b01d267c70c87d0960d6c5937a97c
Timo von Holtz <tvh@tvholtz.de>
2023-09-17 21:00:09 -0500

    Add tests for subscription endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,5 @@
 fn spawn_app() -> String {
-    let listener =
-        std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind to address");
 
     let port = listener.local_addr().unwrap().port();
 
@@ -13,7 +12,6 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
-
     let client = reqwest::Client::new();
 
     let response = client
@@ -24,4 +22,45 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", address))
+        .header("content-type", "application/x-www-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", address))
+            .header("content-type", "application/x-www-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect(error_message);
+
+        assert_eq!(400, response.status().as_u16());
+    }
 }
